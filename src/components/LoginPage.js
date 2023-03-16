430d3d0cc04276c954c7ff47adeb984332812aa4
Shari McRae <sharimcrae@protonmail.com>
2023-03-15 20:55:43 -0500

    incorporated the App.js file and deleted the App.css file

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -3,7 +3,7 @@ import LoginForm from "./LoginForm";
 
 export default function LoginPage() {
   return (
-    <div className="d-flex align-items-center formContainerHeight" >
+    <div className="d-flex align-items-center formContainerHeight">
       <LoginForm />
     </div>
   );
